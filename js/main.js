1743d0e20fb39b51a1da1fd2aa18fb71eac51660
Julius <Julius>
2020-04-24 17:20:35 +0300

    Run numbers | step 1

--- js/main.js
+++ js/main.js
@@ -132,10 +132,31 @@ $(document).ready(function () {
     // tapClose();
 
 
+    // Clock 
+
+    // const time = 50;
+    // const step = 10;
+
+    function runNum(num, elem, time,step) {
+        let l = document.querySelector('#' + elem)
+        n = 0;
+        let t = Math.round(time / (num / step) );
+        let interval = setInterval(() => {
+            n = n + step;
+            if (n >= num){
+                clearInterval(interval);
+               
+            } 
+            l.innerHTML = n;
+            
+        },
+        t);
+    }
+    setTimeout(function() { runNum(9450, 'c3', 1, 10) }, 1000);
+    
     
 
-
-
+    // close tap menu
 
     let tap = document.querySelector('body');
 
